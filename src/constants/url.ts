4da073969bea4b096e1eea8fef715365fa47e3d9
Mertcan Kose <mertcankose1106@gmail.com>
2025-10-19 23:40:44 +0300

    fix: email

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,3 @@
 export const playStore = "https://apps.apple.com/tr/app/ai-rap-music-generator-flow-ai/id6740488650";
 export const appStore = "https://apps.apple.com/tr/app/ai-rap-music-generator-flow-ai/id6740488650";
-export const contactEmail = "contact@mertcankose.com";
+export const contactEmail = "mertcankose142@gmail.com";
